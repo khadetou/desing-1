e0edfeabec38f36c2d28086e347b37fff1847cdc
khadetou <khadetou96@gmail.com>
2022-01-04 23:35:03 +0000

    header

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -10,30 +10,59 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          Center(
-            child: Text(
-              'Hello, world!',
-              style: TextStyle(
-                fontSize: 50,
-                // fontWeight: FontWeight.w900,
-              ),
-            ),
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 240,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Find the stuff you love.",
+                            style: TextStyle(
+                              height: 1.2,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            "Take a look at we found for you!",
+                            style: TextStyle(
+                              fontSize: 16,
+                              height: 1.2,
+                              fontWeight: FontWeight.normal,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage("assets/images/avatar.png")),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-          Center(
-            child: SizedBox(
-              height: 40,
-              width: 40,
-              child: CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/images/avatar.png'),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
